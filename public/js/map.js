567021a85e051ec91ade0dfd3963ff941ef79bb0
digi0ps <sriru1998@gmail.com>
2018-12-16 02:53:23 +0530

    Route calculation between ambulance and user.

--- public/js/map.js
+++ public/js/map.js
@@ -1,19 +1,114 @@
 console.log("hxi")
 
-const setUserPosition = ({ coords }) => {
-    console.log(coords)
+let map, directionsDisplay, directionsService;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("gmap"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 13,
+  });
 
-    // Init Map
+  directionsDisplay = new google.maps.DirectionsRenderer()
+  directionsService = new google.maps.DirectionsService()
 
-    // Plot user's location on map
+  directionsDisplay.setMap(map)
 
-    // Subscribe to ambulance data
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+        const pos = {
+          lat: latitude,
+          lng: longitude,
+        };
 
-    // Plot ambulance on map
+        // Plot user marker
+        // plotUser(pos)
+        getAmbulanceData(pos)
+        // Fetch and mark ambulance marker
+      },
+      error => {
+        console.error(error);
+      },
+    );
+  }
+}  
+/*
+// const plotUser = (pos) => {
+//     const infowindow = new google.maps.InfoWindow();
+//     infowindow.setPosition(pos);
+//     infowindow.setContent("Drone location");
 
-    // Route ambulance to user
+//     const userMarker = new google.maps.Marker({
+//       position: pos,
+//       map: map,
+//     });
 
-    // Live update
+//     map.panTo(pos);
+// } */
+
+const getAmbulanceData = (user_pos) => {
+    const assigned_amb = localStorage.getItem("assigned_amb")
+    const database = firebase.database()
+    database.ref(assigned_amb + "/GPS").once("value").then(function(snapshot) {
+        const val = snapshot.val()
+        const amb_pos = {
+            lat: val.latitude,
+            lng: val.longitude,
+        }
+        // plotAmbulance(amb_pos)
+        plotRoute(amb_pos, user_pos)
+    })
 }
 
-getGeolocation(setUserPosition)
+const plotRoute = (ambulance, user) => {
+
+    const request = {
+        origin: ambulance,
+        destination: user,
+        travelMode: 'DRIVING',
+    }
+
+    directionsService.route(request, (result, status) => {
+        if (status=='OK'){
+            directionsDisplay.setDirections(result)
+            console.log(result)
+        }
+    })
+}
+
+// const plotAmbulance = (pos) => {
+//     const ambMarker = new google.maps.Marker({
+//         position: pos,
+//         map: map,
+//       });
+// }
+
+// const plotUserLocation = (pos) => {
+//     const infowindow = new google.maps.InfoWindow();
+//         infowindow.setPosition(pos);
+//         infowindow.setContent("User location");
+
+//         const userMarker = new google.maps.Marker({
+//           position: pos,
+//           map: map,
+//         });
+// }
+
+// const setUserPosition = ({ coords }) => {
+//     console.log(coords)
+//     const pos = {
+//         lat: coords.latitude,
+//         lng: coords.longitude,
+//     }
+//     // Plot user's location on map
+//     plotUserLocation(pos)
+//     // Subscribe to ambulance data
+
+//     // Plot ambulance on map
+
+//     // Route ambulance to user
+
+//     // Live update
+// }
+
+// getGeolocation(setUserPosition)
