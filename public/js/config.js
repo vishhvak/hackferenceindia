fd6f118a2932de129fbec26ec992d98b815c9bc4
digi0ps <sriru1998@gmail.com>
2018-12-16 00:25:51 +0530

    Create config common.

---
+++ public/js/config.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCo-fsQoqEwUGSdNK3huCH_cU8172JSiXE",
+    authDomain: "ambulance-d528c.firebaseapp.com",
+    databaseURL: "https://ambulance-d528c.firebaseio.com",
+    projectId: "ambulance-d528c",
+    storageBucket: "",
+    messagingSenderId: "836720116918"
+};
+
+firebase.initializeApp(FIREBASE_CONFIG);
