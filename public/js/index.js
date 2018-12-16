a6e4c7331f8d94f9494583d82872225027c565fe
Vishhvak Srinivasan <dudelikesmath@gmail.com>
2018-12-16 12:39:07 +0530

    Authentication started

--- public/js/index.js
+++ public/js/index.js
@@ -5,14 +5,41 @@ function writeNewPost(regdata) {
         ...regdata,
         status: "free",
         currentPatient: null,
-    }, function(error) {
-    if (error) {
-      alert('Error!');
-    } else {
-      localStorage.setItem('ambulance_id', newPostKey)
-      localStorage.setItem('driver_name', regdata.name)
-      window.location.replace('ambulance.html')
-    }
-  });
+    }, function (error) {
+        if (error) {
+            alert('Error!');
+        } else {
+            localStorage.setItem('ambulance_id', newPostKey)
+            localStorage.setItem('driver_name', regdata.name)
+            window.location.replace('ambulance.html')
+        }
+    });
 }
 
+function auth(phone) {
+    var phoneNumber = phone
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
+        'size': 'invisible',
+        'callback': function (response) {
+            // reCAPTCHA solved, allow signInWithPhoneNumber.
+            onSignInSubmit();
+        }
+    });
+    var appVerifier = window.recaptchaVerifier;
+    firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
+        .then(function (confirmationResult) {
+            confirmationResult = "Enter the OTP sent in the field"
+            window.confirmationResult = confirmationResult;
+        }).catch(function (error) {
+            alert("Error sending OTP");
+        });
+    var code = document.getElementById("otp")
+    confirmationResult.confirm(code).then(function (result) {
+        // User signed in successfully.
+        var user = result.user;
+        window.
+    }).catch(function (error) {
+        // User couldn't sign in (bad verification code?)
+        // ...
+    });
+}
